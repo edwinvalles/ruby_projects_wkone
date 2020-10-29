2fad2b9171f2e0be2811264471bda8bef5102ae5
edwinvalles <edwinvalles@gmail.com>
2020-10-29 20:05:01 +0800

    two seatworks completed oct 29 session

--- ruby_three.rb
+++ ruby_three.rb
@@ -16,3 +16,26 @@ classic.read
 local = Books.new('Rizal','Noli Me Tangere','brown')
 local.read
 
+
+
+class Profile
+    attr_accessor :full_name
+    attr_accessor :age
+    attr_accessor :work
+     
+    def initialize(full_name, age, address, work)
+        @full_name = full_name
+        @age = age
+        @address = address
+        @work = work
+    end
+  end
+  
+  my_profile = Profile.new('Ana', 18, 'Bulacan', 'Instructor')
+  
+  puts my_profile.full_name
+  my_profile.full_name = 'Analyn Cajocson'
+  
+  my_profile.age = 25
+  my_profile.work = 'Software Engineer'
+  
