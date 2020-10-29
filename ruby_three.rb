b8c5ca93c5a2fed94fa8fc5baf73a4e463d8b6ff
edwinvalles <edwinvalles@gmail.com>
2020-10-29 20:22:59 +0800

    updated 2 exercises for Oct 29

--- ruby_three.rb
+++ ruby_three.rb
@@ -22,7 +22,7 @@ class Profile
     attr_accessor :full_name
     attr_accessor :age
     attr_accessor :work
-     
+
     def initialize(full_name, age, address, work)
         @full_name = full_name
         @age = age
@@ -33,9 +33,12 @@ class Profile
   
   my_profile = Profile.new('Ana', 18, 'Bulacan', 'Instructor')
   
-  puts my_profile.full_name
   my_profile.full_name = 'Analyn Cajocson'
-  
+  puts my_profile.full_name
+
   my_profile.age = 25
   my_profile.work = 'Software Engineer'
+  puts my_profile.age
+  puts my_profile.work
+  
   
