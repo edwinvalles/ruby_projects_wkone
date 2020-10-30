cef6092260e134f080acb8c205c2d35375407a42
edwinvalles <edwinvalles@gmail.com>
2020-10-30 17:16:30 +0800

    Completes confection assignment

---
+++ ruby_four.rb
@@ -0,0 +1,30 @@
+module Baking
+    def prepare 
+        puts 'Baking at 350 degrees for 25 minutes.'
+    end
+end
+
+class Confection
+    include Baking
+end
+
+class Banana_Cake < Confection
+    include Baking
+end
+
+class Cupcake < Confection
+    include Baking
+    def frosting 
+        puts 'Applying frosting'
+    end
+end
+
+chiffon = Confection.new
+chiffon.prepare
+
+with_fudge = Banana_Cake.new
+with_fudge.prepare
+
+red_velvet = Cupcake.new
+red_velvet.prepare
+red_velvet.frosting
