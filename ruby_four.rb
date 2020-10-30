3f5d0e79d4928165ee3da65ab8b1473996df748c
edwinvalles <edwinvalles@gmail.com>
2020-10-30 19:14:53 +0800

    Updated with Analyn's comments

--- ruby_four.rb
+++ ruby_four.rb
@@ -1,21 +1,47 @@
-module Baking
+# module Baking
+#     def prepare 
+#         puts 'Baking at 350 degrees for 25 minutes.'
+#     end
+# end
+
+# class Confection
+#     include Baking
+# end
+
+# class Banana_Cake < Confection
+#     include Baking
+# end
+
+# class Cupcake < Confection
+#     include Baking
+#     def frosting 
+#         puts 'Applying frosting'
+#     end
+# end
+
+# chiffon = Confection.new
+# chiffon.prepare
+
+# with_fudge = Banana_Cake.new
+# with_fudge.prepare
+
+# red_velvet = Cupcake.new
+# red_velvet.prepare
+# red_velvet.frosting
+
+class Confection
     def prepare 
         puts 'Baking at 350 degrees for 25 minutes.'
     end
 end
 
-class Confection
-    include Baking
-end
-
 class Banana_Cake < Confection
-    include Baking
 end
 
 class Cupcake < Confection
-    include Baking
-    def frosting 
-        puts 'Applying frosting'
+    def prepare
+        super
+        puts 'Applying frosting.'
     end
 end
 
@@ -27,4 +53,3 @@ with_fudge.prepare
 
 red_velvet = Cupcake.new
 red_velvet.prepare
-red_velvet.frosting
